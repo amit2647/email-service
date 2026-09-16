97b0660932503c5c70e6c8b5af9386820e025772
amitmahorkarAptsi <amahorkar@aptsi.com>
2026-09-17 00:56:34 +0530

    feat: add CORS support to the email service

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const cors = require("cors");
 const emailRoutes = require("./routes/emailRoutes");
 const emailAccountRoutes = require("./routes/emailAccountRoutes");
 const requestLogger = require("./middleware/requestLogger");
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   express.json({
